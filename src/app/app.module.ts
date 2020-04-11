5b59c35fda872befb810d4643689c25968cc046d
Yahor Pamakha <yahorp@google.com>
2020-04-11 20:38:25 +0300

    rename home folder on course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './pages/home/components/footer/footer.component';
-import { HeaderComponent } from './pages/home/components/header/header.component';
-import { NavigationComponent } from './pages/home/components/navigation/navigation.component';
-import { FakeLogoComponent } from './pages/home/components/fake-logo/fake-logo.component';
-import { AvailableCoursesComponent } from './pages/home/components/available-courses/available-courses.component';
-import { SearchComponent } from './pages/home/components/search/search.component';
-import { AddCourseButtonComponent } from './pages/home/components/add-course-button/add-course-button.component';
-import { MainContentWrapperComponent } from './pages/home/components/main-content-wrapper/main-content-wrapper.component';
+import { FooterComponent } from './pages/course/components/footer/footer.component';
+import { HeaderComponent } from './pages/course/components/header/header.component';
+import { NavigationComponent } from './pages/course/components/navigation/navigation.component';
+import { FakeLogoComponent } from './pages/course/components/fake-logo/fake-logo.component';
+import { AvailableCoursesComponent } from './pages/course/components/available-courses/available-courses.component';
+import { SearchComponent } from './pages/course/components/search/search.component';
+import { AddCourseButtonComponent } from './pages/course/components/add-course-button/add-course-button.component';
+import { MainContentWrapperComponent } from './pages/course/components/main-content-wrapper/main-content-wrapper.component';
 
 @NgModule({
   declarations: [
